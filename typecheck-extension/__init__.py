11566fcfabd7f89d33a4b9abbc1af39036831ce2
Alan Braz <braz.alan@ibm.com>
2018-06-29 11:11:19 -0400

    remove timer events

--- typecheck-extension/__init__.py
+++ typecheck-extension/__init__.py
@@ -28,11 +28,11 @@ timer = LineWatcher()
 
 
 def load_ipython_extension(ipython):
-    ipython.events.register('pre_run_cell', timer.start)
-    ipython.events.register('post_run_cell', timer.stop)
+    # ipython.events.register('pre_run_cell', timer.start)
+    # ipython.events.register('post_run_cell', timer.stop)
     ipython.register_magics(TypeCheck)
 
 
-def unload_ipython_extension(ipython):
-    ipython.events.unregister('pre_run_cell', timer.start)
-    ipython.events.unregister('post_run_cell', timer.stop)
+# def unload_ipython_extension(ipython):
+    # ipython.events.unregister('pre_run_cell', timer.start)
+    # ipython.events.unregister('post_run_cell', timer.stop)
