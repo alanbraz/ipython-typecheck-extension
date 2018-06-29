ba05c7e9faa4b3843d1e4042d7a2d4eb17838a41
Alan Braz <braz.alan@ibm.com>
2018-06-29 10:43:43 -0400

    remove last line

--- typecheck-extension/__init__.py
+++ typecheck-extension/__init__.py
@@ -34,4 +34,3 @@ def load_ipython_extension(ipython):
 def unload_ipython_extension(ipython):
     ipython.events.unregister('pre_run_cell', timer.start)
     ipython.events.unregister('post_run_cell', timer.stop)
-    #ipython.unregister_magics(TypeCheck)
