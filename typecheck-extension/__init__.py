28510a491ff7a28651438ace34bd1412ad92a290
Alan Braz <braz.alan@ibm.com>
2018-06-29 10:38:28 -0400

    ignore timer for testing

--- typecheck-extension/__init__.py
+++ typecheck-extension/__init__.py
@@ -1,10 +1,11 @@
-from .typecheck import TypeCheck, timer
+from .typecheck import TypeCheck
 
 def load_ipython_extension(ipython):
-    ipython.events.register('pre_run_cell', timer.start)
-    ipython.events.register('post_run_cell', timer.stop)
+    #ipython.events.register('pre_run_cell', timer.start)
+    #ipython.events.register('post_run_cell', timer.stop)
     ipython.register_magics(TypeCheck)
 
 def unload_ipython_extension(ipython):
-    ipython.events.unregister('pre_run_cell', timer.start)
-    ipython.events.unregister('post_run_cell', timer.stop)
+    #ipython.events.unregister('pre_run_cell', timer.start)
+    #ipython.events.unregister('post_run_cell', timer.stop)
+    ipython.unregister_magics(TypeCheck)
