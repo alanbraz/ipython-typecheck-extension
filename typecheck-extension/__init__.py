df459f74aa4fb10d3dfc7b44a58760946f091144
Alan Braz <braz.alan@ibm.com>
2018-06-29 11:22:49 -0400

    register timer events

--- typecheck-extension/__init__.py
+++ typecheck-extension/__init__.py
@@ -1,14 +1,14 @@
 """Experimental packege for IPython extension."""
-
 from .typecheck import TypeCheck
+from .autotime import timer
 
 
 def load_ipython_extension(ipython):
-    # ipython.events.register('pre_run_cell', timer.start)
-    # ipython.events.register('post_run_cell', timer.stop)
+    ipython.events.register('pre_run_cell', timer.start)
+    ipython.events.register('post_run_cell', timer.stop)
     ipython.register_magics(TypeCheck)
 
 
-# def unload_ipython_extension(ipython):
-    # ipython.events.unregister('pre_run_cell', timer.start)
-    # ipython.events.unregister('post_run_cell', timer.stop)
+def unload_ipython_extension(ipython):
+    ipython.events.unregister('pre_run_cell', timer.start)
+    ipython.events.unregister('post_run_cell', timer.stop)
