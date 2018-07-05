eaa1e7a655e053ce6e773638c4858d9dd8d2a555
Alan Braz <braz.alan@ibm.com>
2018-07-05 15:30:09 -0400

    move typechecker to post_run_cell and check over all previous cells code that had no error

--- typecheck/__init__.py
+++ typecheck/__init__.py
@@ -1,18 +1,18 @@
 """Experimental packege for IPython extension."""
-# from .typecheck import TypeCheck
-from .autotime import timer
-from .typecheck_magic import TypeCheck as tcMagic
+from .typecheck import TypeCheck
+# from .autotime import timer
+# from .typecheck_magic import TypeCheck as tcMagic
 
-__version__ = "0.4"
+__version__ = "0.5"
 
 def load_ipython_extension(ipython):
-    ipython.events.register('pre_run_cell', timer.start)
-    ipython.events.register('post_run_cell', timer.stop)
+    # ipython.events.register('pre_run_cell', timer.start)
+    # ipython.events.register('post_run_cell', timer.stop)
     tc = TypeCheck(ipython)
-    # ipython.events.register('pre_run_cell', tc.check)
-    ipython.register_magics(tcMagic)
+    ipython.events.register('post_run_cell', tc.check)
+    # ipython.register_magics(tcMagic)
 
-def unload_ipython_extension(ipython):
-    ipython.events.unregister('pre_run_cell', timer.start)
-    ipython.events.unregister('post_run_cell', timer.stop)
+# def unload_ipython_extension(ipython):
+    # ipython.events.unregister('pre_run_cell', timer.start)
+    # ipython.events.unregister('post_run_cell', timer.stop)
     # ipython.events.unregister('pre_run_cell', tc.check)
--- typecheck/typecheck.py
+++ typecheck/typecheck.py
@@ -1,40 +1,24 @@
-"""
-Add mypy type-checking cell magic to jupyter/ipython.
-Save this script to your ipython profile's startup directory.
-IPython's directories can be found via `ipython locate [profile]` to find the current ipython directory and ipython profile directory, respectively.
-For example, this file could exist on a path like this on mac:
-/Users/yourusername/.ipython/profile_default/startup/typecheck.py
-where /Users/yourusername/.ipython/profile_default/ is the ipython directory for
-the default profile.
-The line magic is called "typecheck" to avoid namespace conflict with the mypy
-package.
-"""
 class TypeCheck(object):
-    """
-    Class that implements mypy call.
-    """
+
     def __init__(self, ip):
         self.shell = ip
+        self.ok_cells = []
 
-    def check(self, info):
-        print("typecheck...")
-        print("user_ns", self.shell.user_ns)
+    def check(self):
+        # print("typecheck...")
         from mypy import api
         import sys
-        """
-        Run the following cell though mypy.
-        Any parameters that would normally be passed to the mypy cli
-        can be passed on the first line, with the exception of the
-        -c flag we use to pass the code from the cell we want to execute
-        mypy stdout and stderr will print prior to output of cell.
-        If there are no conflicts, mnothing will be printed by mypy.
-        """
-        mycell = info.raw_cell
-        # TODO send all previous code cells, instead of current cell
-        mypy_result = api.run(['-c', mycell, '--ignore-missing-imports'])
-        if mypy_result[0]:
-            print("TypeCheck: " + mypy_result[0], file=sys.stderr)
-        if mypy_result[1]:
-            print("TypeCheck: " + mypy_result[1], file=sys.stderr)
-
-        # TODO try to abort cell to run if typecheck error, return False or return None, None don't work
+        cells = self.shell.user_ns["In"]
+        # print("cells", cells)
+        current_cell = cells[-1]
+        # print("current_cell", current_cell)
+        cells_to_run = "\n".join(self.ok_cells + [current_cell])
+        # print("cells_to_run", cells_to_run)
+        mypy_result = api.run(['-c', cells_to_run, '--ignore-missing-imports'])
+        if mypy_result[0] or mypy_result[1]:
+            if mypy_result[0]:
+                print("TypeCheck: " + mypy_result[0], file=sys.stderr)
+            if mypy_result[1]:
+                print("TypeCheck: " + mypy_result[1], file=sys.stderr)
+        else:
+            self.ok_cells.append(current_cell)
