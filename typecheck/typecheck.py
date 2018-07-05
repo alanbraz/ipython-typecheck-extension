e086162dee8de5f3816574c43c5e3484a872f6f0
Alan Braz <braz.alan@ibm.com>
2018-07-05 16:50:34 -0400

    removing python command lines from current cell

--- typecheck/typecheck.py
+++ typecheck/typecheck.py
@@ -12,27 +12,28 @@ class TypeCheck(object):
         # print("cells", cells)
         current_cell = cells[-1]
         print("current_cell", current_cell)
-        cells_to_run = "\n".join(self.ok_cells + [current_cell])
-        print("cells_to_run", cells_to_run)
-        mypy_result = api.run(['-c', cells_to_run, '--ignore-missing-imports'])
-        error = None
-        if mypy_result[0]:
-            error = mypy_result[0]
-        if mypy_result[1]:
-            error = mypy_result[1]
-        # print("error", error)
-        if error is not None:
-            parts = error.split(":")
-            print("parts", parts)
-            line = int(parts[1])
-            print("line", line)
-            print("self.ok_cells lines", len(self.ok_cells))
-            print("cells_to_run split", cells_to_run.split("\n"))
-            print("cells_to_run lines", len(cells_to_run.split("\n")))
-            print("current_cell split", current_cell.split("\n"))
-            print("current_cell lines", len(current_cell.split("\n")))
-            parts[1] = (len(self.ok_cells) - line)
-            error = (":").join(parts)
-            print("TypeCheck: " + error, file=sys.stderr)
-        else:
-            self.ok_cells.append(current_cell)
+        current_cell_lines = []
+        for line in current_cell.split("\n"):
+            if "get_ipython" not in line:
+                current_cell_lines.append(line)
+        if len(current_cell_lines) > 0:
+            current_cell = "\n".join(current_cell_lines)
+            cells_to_run = "\n".join(self.ok_cells + [current_cell])
+            print("cells_to_run", cells_to_run)
+            mypy_result = api.run(['-c', cells_to_run, '--ignore-missing-imports'])
+            error = None
+            if mypy_result[0]:
+                error = mypy_result[0]
+            if mypy_result[1]:
+                error = mypy_result[1]
+            # print("error", error)
+            if error is not None:
+                parts = error.split(":")
+                print("parts", parts)
+                line = int(parts[1])
+                print("self.ok_cells lines", len(self.ok_cells))
+                parts[1] = str(line - len(self.ok_cells))
+                error = (":").join(parts)
+                print("TypeCheck: " + error, file=sys.stderr)
+            else:
+                self.ok_cells.append(current_cell)
