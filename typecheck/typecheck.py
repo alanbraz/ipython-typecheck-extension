67837d06b3637a792322409d6294ef0c796372fe
Alan Braz <braz.alan@ibm.com>
2018-07-06 09:47:09 -0400

    add --show-column-numbers and default import

--- typecheck/typecheck.py
+++ typecheck/typecheck.py
@@ -2,7 +2,7 @@ class TypeCheck(object):
 
     def __init__(self, ip):
         self.shell = ip
-        self.ok_cells = []
+        self.ok_cells = [ "import os", "os.environ['MYPYPATH'] = os.environ['PYTHONPATH']" ]
 
     def check(self):
         # print("typecheck...")
@@ -21,7 +21,7 @@ class TypeCheck(object):
             current_cell = "\n".join(current_cell_lines)
             cells_to_run = "\n".join(self.ok_cells + [current_cell])
             # print("cells_to_run", cells_to_run)
-            mypy_result = api.run(['-c', cells_to_run, '--ignore-missing-imports'])
+            mypy_result = api.run(['-c', cells_to_run, '--ignore-missing-imports', '--show-column-numbers'])
             error = None
             if mypy_result[0]:
                 error = mypy_result[0]
